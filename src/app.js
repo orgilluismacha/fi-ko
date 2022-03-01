eeb43c962cf9c901e3c04ca1c492b99db11e6cc9
Luis <orgilluismacha@gmail.com>
2022-03-01 20:14:42 +0100

    Test

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+const FiKoDashboardPage = () =>(
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () =>(
+  <div>
+    This is my AddExpense component
+  </div>
+);
+const EditExpensePage = () =>(
+  <div>
+    This is my Edit component
+  </div>
+);
+const HelpPage = () =>(
+  <div>
+    This is my Help component
+  </div>
+);
+
+
+
+const routes = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<FiKoDashboardPage/>} exact={true}/>
+      <Route path="/create" element={<AddExpensePage/>}/>
+      <Route path="/edit" element={<EditExpensePage/>}/>
+      <Route path="/help" element={<HelpPage/>}/>
+    </Routes>
+  </BrowserRouter>
+
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: "/"
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    },{
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static : {
+      directory : path.join(__dirname, 'public')
+    },
+    historyApiFallback: true
+  }
+};
