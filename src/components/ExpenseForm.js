67915e4dd74689288c6073cdcd0538296f4adb5a
orgilluismacha <orgilluismacha@gmail.com>
2022-04-29 17:42:26 +0200

    remove gitignore files

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -32,7 +32,6 @@ export default class ExpenseForm extends React.Component {
     if(createdAt){
       this.setState(() => ({ createdAt}));
     }
-
   };
 
   onFocusChange = ({ focused }) => {
@@ -53,7 +52,7 @@ export default class ExpenseForm extends React.Component {
     
     if(!this.state.description || !this.state.amount){
       // Set error state equal to "Please provide description and amount."
-      this.setState(() => ({error: 'ADD WHAT??'}));
+      this.setState(() => ({error: 'Please check if the values you have entered is valid.'}));
       console.log('bruh');
     } else {
       // Clear the error
@@ -100,7 +99,7 @@ export default class ExpenseForm extends React.Component {
         <textarea
           value={this.state.note}
           onChange={this.onNoteChange}
-          placeholder="Add a note for your expense (optional)">
+          placeholder="Addd a note for your expense (optional)">
         </textarea>
 
         <button>Add</button>
@@ -108,5 +107,4 @@ export default class ExpenseForm extends React.Component {
     </div>
     )
   }
-
 };
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
+import EditItemModal from "./EditItemModal.js";
 import { connect } from "react-redux";
 import { removeExpense } from '../actions/expenses.js'
 
 const ExpenseListItem = ({dispatch, id, description, amount,createdAt}) => (
   <div>
     {description} {amount}Ft {createdAt}
+    
+    <button>Edit</button>
+
     <button onClick={(e) => {
     dispatch(removeExpense({id}));
       console.log(id);
 
     }}>Remove</button>
+    <EditItemModal/>
   </div>
 );
 
