30c5b6be185832b793e6ead2e53fc764cbf338fd
orgilluismacha <orgilluismacha@gmail.com>
2022-05-01 17:29:26 +0200

    Set up formatting for currency and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,20 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { removeExpense } from '../actions/expenses.js'
+import { removeExpense } from '../actions/expenses.js';
+import moment from "moment";
+import numeral from "numeral";
 
-const ExpenseListItem = ({dispatch, id, description, amount,createdAt}) => (
+numeral.register('locale', 'hu', {
+  delimiters: {
+    thousands: ' ',
+    decimal: ','
+  },
+  abbreviations: {
+    thousand: 'E',  // ezer
+    million: 'M',   // millió
+    billion: 'Mrd', // milliárd
+    trillion: 'T'   // trillió
+  },
+  ordinal: function (number) {
+    return '.';
+  },
+  currency: {
+    symbol: ' Ft'
+  }
+});
+
+numeral.locale('hu');
+
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       {description}
     </Link>
-    {amount}Ft {createdAt}
+    {numeral(amount).format('0,0[.]00 $')} {moment(createdAt).format('MMMM Do, YYYY')}
     <button onClick={(e) => {
-    dispatch(removeExpense({id}));
+      dispatch(removeExpense({ id }));
       console.log(id);
 
     }}>Remove</button>
-    
+
   </div>
 );
 
