5864873ae82d5bd9354f7548f22c378ebcd7c3b4
orgilluismacha <orgilluismacha@gmail.com>
2022-05-18 15:55:43 +0200

    Added fetch function for firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -52,3 +52,32 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+
+//SET_EXPENSES
+
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return get(child(ref(db), 'expenses')).then((snapshot) => {
+      if (snapshot.exists()) {
+        const expenses = []
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val(),
+          });
+        });
+        dispatch(setExpenses(expenses));
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+}
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import  './firebase/firebase';
@@ -13,9 +13,9 @@ import 'normalize.css/normalize.css';
 const store = configureStore();
 
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 5400, createdAt:5843298400}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 7600, createdAt:10432800}));
-store.dispatch(addExpense({ description: 'Electricity bill', amount: 3000, createdAt:2482380748300}));
+// store.dispatch(addExpense({ description: 'Water bill', amount: 5400, createdAt:5843298400}));
+// store.dispatch(addExpense({ description: 'Gas bill', amount: 7600, createdAt:10432800}));
+// store.dispatch(addExpense({ description: 'Electricity bill', amount: 3000, createdAt:2482380748300}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
@@ -28,6 +28,10 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading..</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
 
 
